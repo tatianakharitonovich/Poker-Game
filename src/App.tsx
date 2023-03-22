491360ab5dfaa404b323e90e744d2cf8b86c0d79
Tanya Kharitonovich <k.tanya@tut.by>
2023-03-22 10:08:34 +0100

    feat: add `App`

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./App.css";
+import { GameLayout } from "./components/GameLayout";
+import { RegistrationForm } from "./components/registration-form/RegistrationForm";
+import { Gender } from "./types";
+
+export const App: React.FC = () => {
+    const [userName, setUserName] = useState("");
+    const [gender, setGender] = useState<Gender>();
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    return (
+        <div className="App">
+            <div className="App-wrap">
+                {!isSubmit ?
+                    (
+                        <RegistrationForm
+                            userName={userName}
+                            gender={gender}
+                            userNameHandler={setUserName}
+                            genderHandler={setGender}
+                            submitHandler={setIsSubmit}
+                        />
+                    )
+                    : (
+                        <GameLayout userName={userName} gender={gender} />
+                    )
+               }
+            </div>
+        </div>
+    );
+};
